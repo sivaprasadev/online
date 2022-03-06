7b8373e6c2df8fa5b076c42ca6b18e79ab86e8c7
sivaprasad e v <sivaprasad.ev@capfront.in>
2022-03-06 12:19:42 +0530

    project in mind section and testimonial completed

--- JS/main.js
+++ JS/main.js
@@ -98,7 +98,7 @@ modalCloseBtn.forEach((modalClose) => {
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
 	cssMode: true,
 	loop: true,
 	navigation: {
@@ -113,6 +113,23 @@ let swiper = new Swiper('.portfolio__container', {
 });
 
 /*==================== TESTIMONIAL ====================*/
+
+let swiperTestimonial = new Swiper('.testimonial__container', {
+	loop: true,
+	grabCursor: true,
+	spaceBetween: 48,
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true
+	},
+	breakpoints: {
+		568: {
+			slidesPerView: 2
+		}
+	}
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
