c95a8d58088775c1d44706fa524d9b23e4e1df53
sivaprasad e v <sivaprasad.ev@capfront.in>
2022-03-06 10:02:10 +0530

    services section completed

--- JS/main.js
+++ JS/main.js
@@ -74,6 +74,28 @@ tabs.forEach((tab) => {
 
 /*==================== SERVICES MODAL ====================*/
 
+const modalView = document.querySelectorAll('.services__modal');
+const viewMoreBtn = document.querySelectorAll('.services__button');
+const modalCloseBtn = document.querySelectorAll('.services__modal__close');
+
+let modal = function (modalClick) {
+	modalView[modalClick].classList.add('active-modal');
+};
+
+viewMoreBtn.forEach((modalBtn, i) => {
+	modalBtn.addEventListener('click', () => {
+		modal(i);
+	});
+});
+
+modalCloseBtn.forEach((modalClose) => {
+	modalClose.addEventListener('click', () => {
+		modalView.forEach((modalView) => {
+			modalView.classList.remove('active-modal');
+		});
+	});
+});
+
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 /*==================== TESTIMONIAL ====================*/
